c3b5b27a3a13dd196f6cacc6f1989e2f09dddcf1
it21096570 <it21096570@my.sliit.lk>
2023-10-13 12:36:00 +0530

    commit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,21 +16,6 @@ import ChangePass from './components/ChangePass';
 import Profile from './components/Profile';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/Footer';
-import Editor from './components/Editor'
-import AllPost from './components/AllPost';
-import CreatePost from './components/CreatePost';
-import SelectedPost from './components/SelectedPost';
-import UpdatePost from './components/UpdatePost';
-import About from './components/About';
-import CreateCourse from './components/CreateCourse';
-import ViewCoursesAdmin from './components/ViewCoursesAdmin';
-import UpdateCourse from './components/UpdateCourse'; // Import the UpdateCourse component
-import ViewCoursesUser from './components/ViewCoursesUser';
-import ViewTutorialsUser from './components/ViewTutorialsUser';
-import ViewTutorialsAdmin from './components/ViewTutorialsAdmin';
-import CreateTutorial from './components/CreateTutorial';
-import UpdateTutorial from './components/UpdateTutorial';
-
 
 
 import Cookies from 'js-cookie';
@@ -80,13 +65,6 @@ function App() {
                 <Route path='/getUsers' element={<ViewAllUsers />} />
                 <Route path='/updateUsers/:userId' element={<UpdateUser />} />
 
-                <Route path='/createCourse' element={<CreateCourse />} />
-                <Route path='/createTutorial/:courseId/:courseName' element={<CreateTutorial />} />
-                <Route path='/getCourseAdmin' element={<ViewCoursesAdmin />} />
-                <Route path='/getTutorialAdmin/:courseId/:courseName' element={<ViewTutorialsAdmin />} />
-                <Route path='/courses/update/:courseId' element={<UpdateCourse />} />
-                <Route path='/tutorials/update/:tutorialId/:courseId/:courseName' element={<UpdateTutorial />} />
-
 
               </Routes>
             </Router>
@@ -100,16 +78,8 @@ function App() {
 
                 {/*<Route exact path='/userHome/:token/:role' element={<UserHome />} />*/}
                 <Route path='/userHome' element={<UserHome />} />
-                <Route path='/editor' element={<Editor />} />
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/resetPassword' element={<ResetPassword />} />
-                <Route path="/allpost" element={<AllPost />} />
-                <Route path="/post/:postId" element={<SelectedPost />} />
-                <Route path="/editpost/:postId" element={<UpdatePost />} />
-                <Route path="/createpost" element={<CreatePost />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/viewCourses" element={<ViewCoursesUser />} />
-                <Route path="/viewTutorials/:courseId/:courseName" element={<ViewTutorialsUser />} />
 
 
 
