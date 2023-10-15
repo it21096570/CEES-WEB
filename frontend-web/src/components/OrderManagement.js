03726f7a087f1930432069f355436b15109c0091
IT21191688 <IT21191688@my.sliit.lk>
2023-10-15 14:32:39 +0530

    commit

---
+++ frontend-web/src/components/OrderManagement.js
@@ -0,0 +1,16 @@
+import React from "react"
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+
+export default function OrderManagement() {
+
+
+    return (
+
+
+        <div className="h-screen flex flex-wrap justify-center items-center">
+
+        </div>
+    )
+}
