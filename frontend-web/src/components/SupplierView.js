47af3d3a058531d4fce014b9905caddc2a8e2dcc
IT21071652 <it21071652@my.sliit.lk>
2023-10-16 10:29:26 +0530

    SVedit

--- frontend-web/src/components/SupplierView.js
+++ frontend-web/src/components/SupplierView.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 
 function SupplierView() {
   const [orders, setOrders] = useState([]);
@@ -17,28 +18,32 @@ function SupplierView() {
   }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div style={errorStyle}>Error: {error.message}</div>;
   } else {
     // Filter orders with status "approved"
-    const approvedOrders = orders.filter(order => order.status === "Pending");
+    const approvedOrders = orders.filter(order => order.status === "Rejected");
 
     return (
-      <div className="supplier-view">
-        <h1>Supplier View - Approved Orders</h1>
-        <table>
+      <div style={supplierViewStyle}>
+        <h1 style={titleStyle}>Supplier View - Approved Orders</h1>
+        <table style={tableStyle}>
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Total</th>
-              <th>Status</th>
+              <th style={headerStyle}>Name</th>
+              <th style={headerStyle}>Total</th>
+              <th style={headerStyle}>Status</th>
+              <th style={headerStyle}>Actions</th>
             </tr>
           </thead>
           <tbody>
             {approvedOrders.map(order => (
               <tr key={order._id}>
-                <td>{order.name}</td>
-                <td>${order.total}</td>
-                <td>{order.status}</td>
+                <td style={cellStyle}>{order.name}</td>
+                <td style={cellStyle}>${order.total}</td>
+                <td style={cellStyle}>{order.status}</td>
+                <td>
+                  <Link to={`/confirm-invoice/${order._id}`} style={buttonStyle}>Confirm Invoice</Link>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -49,3 +54,44 @@ function SupplierView() {
 }
 
 export default SupplierView;
+
+// Inline CSS styles
+const supplierViewStyle = {
+  textAlign: 'center',
+  backgroundColor: '#339CFF', // Background color
+  padding: '20px',
+};
+
+const titleStyle = {
+  color: '#4933FF', // Title color
+};
+
+const tableStyle = {
+  width: '100%',
+  borderCollapse: 'collapse',
+  backgroundColor: '#fff', // Table background color
+};
+
+const headerStyle = {
+  padding: '10px',
+  textAlign: 'left',
+  backgroundColor: '#4933FF', // Header background color
+  color: '#fff', // Header text color
+};
+
+const cellStyle = {
+  padding: '10px',
+  borderBottom: '1px solid #ccc', // Border color between rows
+};
+
+const buttonStyle = {
+  backgroundColor: '#4933FF', // Button background color
+  color: '#fff', // Button text color
+  padding: '5px 10px',
+  border: 'none',
+  textDecoration: 'none',
+};
+
+const errorStyle = {
+  color: 'red', // Error text color
+};
