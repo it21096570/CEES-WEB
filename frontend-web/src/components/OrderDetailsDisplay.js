0f161c14408c5bbc89dd919471ba6213c90e93ab
IT21191688 <IT21191688@my.sliit.lk>
2023-10-16 10:08:45 +0530

    approve reject completed

--- frontend-web/src/components/OrderDetailsDisplay.js
+++ frontend-web/src/components/OrderDetailsDisplay.js
@@ -10,7 +10,7 @@ export default function OrderDetailsDisplay() {
     const [orderDetails, setOrderDetails] = useState({});
     const [orderItemDetails, setOrderItemDetails] = useState([]);
 
-    useEffect(function () {
+    useEffect(() => {
         async function fetchData() {
             try {
                 const orderResponse = await axios.get(`http://localhost:8080/order/getOneOrder/${orderId}`);
@@ -26,6 +26,40 @@ export default function OrderDetailsDisplay() {
         fetchData();
     }, [orderId]);
 
+    const managerApproval = (id) => {
+        const newStatus = "Approved"; // Change this to the desired status
+
+        axios.put(`http://localhost:8080/order/updateOrder/${id}`, { status: newStatus })
+            .then((response) => {
+                if (response.status === 200) {
+                    alert(`Order ${orderId} has been ${newStatus}`);
+                    // Optionally, you can update the order status in your frontend data
+                    setOrderDetails({ ...orderDetails, status: newStatus });
+                }
+            })
+            .catch((error) => {
+                console.error('Error updating order status: ', error);
+                alert('Error updating order status: ' + error.message);
+            });
+    };
+
+    const managerReject = (id) => {
+        const newStatus = "Rejected"; // Change this to the desired status
+
+        axios.put(`http://localhost:8080/order/updateOrder/${id}`, { status: newStatus })
+            .then((response) => {
+                if (response.status === 200) {
+                    alert(`Order ${orderId} has been ${newStatus}`);
+                    // Optionally, you can update the order status in your frontend data
+                    setOrderDetails({ ...orderDetails, status: newStatus });
+                }
+            })
+            .catch((error) => {
+                console.error('Error updating order status: ', error);
+                alert('Error updating order status: ' + error.message);
+            });
+    };
+
     // Filter order items based on the search input
     let filteredItems = [];
     if (Array.isArray(orderItemDetails)) {
@@ -36,14 +70,12 @@ export default function OrderDetailsDisplay() {
 
     const totalCost = filteredItems.reduce((total, item) => total + item.itemTotal, 0);
 
-
     function generatePdf() {
         const unit = "pt";
         const size = "A3";
         const orientation = "portrait";
-
         const marginLeft = 400;
-        const margin = 20
+        const margin = 20;
         const doc = new jsPDF(orientation, unit, size);
 
         const title = "Order Table Details";
@@ -74,11 +106,8 @@ export default function OrderDetailsDisplay() {
         doc.save("OrderDetails.pdf");
     }
 
-
-
     return (
         <div className="w-full md:w-3/4 lg:w-4/5 xl:w-5/6 shadow-lg bg-white mx-auto">
-
             <div className="w-52 md:w-2/3 lg:w-1/3 shadow-lg bg-white mx-auto p-2 float-left mt-5">
                 <h1 className="text-lg font-semibold">Order Details</h1>
                 <p className="text-sm">
@@ -87,8 +116,6 @@ export default function OrderDetailsDisplay() {
                     Status: {orderDetails.status}
                 </p>
             </div>
-
-
             <div className="flex justify-end pr-10 pt-10">
                 <div className="flex items-center">
                     <input
@@ -98,21 +125,27 @@ export default function OrderDetailsDisplay() {
                         onChange={(e) => setSearch(e.target.value)}
                         className="mr-4 py-2 px-4 border rounded-lg"
                     />
-
                     <div className="flex items-center space-x-4">
-                        <button className="flex items-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
+                        <button
+                            className="flex items-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
+                            onClick={() => managerApproval(orderDetails._id)}
+                        >
                             <span className="mr-2">Approve</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                             </svg>
                         </button>
-                        <button className="flex items-center bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">
+                        <button className="flex items-center bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+                            onClick={() => managerReject(orderDetails._id)} >
                             <span className="mr-2">Reject</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
-                        <button className="flex items-center bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" onClick={generatePdf} >
+                        <button
+                            className="flex items-center bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                            onClick={generatePdf}
+                        >
                             <span className="mr-2">Print</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19l-7-7 7-7m4 14l7-7-7-7"></path>
@@ -121,7 +154,6 @@ export default function OrderDetailsDisplay() {
                     </div>
                 </div>
             </div>
-
             <table className="w-full shadow-lg bg-white my-4 mx-auto">
                 <thead>
                     <tr>
@@ -146,7 +178,5 @@ export default function OrderDetailsDisplay() {
                 Total Cost: ${totalCost}
             </p>
         </div>
-
-
     );
 }
