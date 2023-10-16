117265af44fd658e69b53fb3bf006df28eefe37d
it21096570 <it21096570@my.sliit.lk>
2023-10-16 16:09:09 +0530

    after create order

--- frontend-web/src/components/UserHome.js
+++ frontend-web/src/components/UserHome.js
@@ -2,11 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../css/home.css'; // Import your custom CSS file
 import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 export default function UserHome() {
+  const navigate = useNavigate();
+
+  const SiteManagerCreateOrder = () => {
+    navigate('/SiteManagerCreateOrder');
+  };
+
   return (
     <div>
       <h1>User Home</h1>
+      <button onClick={SiteManagerCreateOrder}> order </button>
     </div>
   );
 
